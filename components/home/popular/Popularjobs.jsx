09cd67bb4ae9ee6df3fecab0175f54710213b29d
Kevin Cioch <kevin.cioch@gmail.com>
2023-10-19 20:02:46 +0200

    Started implementing Home screen

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,14 +1,48 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { React, useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
+import { useRouter } from "expo-router";
 
-import styles from './popularjobs.style'
+import styles from "./popularjobs.style";
+import { COLORS, SIZES } from "../../../constants";
+import PopularJobCard from "../../common/cards/popular/PopularJobCard";
 
 const Popularjobs = () => {
-  return (
-    <View>
-      <Text>Popularjobs</Text>
-    </View>
-  )
-}
+  const isLoading = false;
+  const error = false;
 
-export default Popularjobs
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popular Jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show All</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator size="large" color={COLORS.primary} />
+        ) : error ? (
+          <Text>Something went wrong</Text>
+        ) : (
+          <FlatList
+            data={[1, 2, 3, 4]}
+            contentContainerStyle={{ columnGap: SIZES.medium }}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            keyExtractor={(item) => item?.job_id}
+            renderItem={({ item }) => <PopularJobCard item={item} />}
+          />
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default Popularjobs;
