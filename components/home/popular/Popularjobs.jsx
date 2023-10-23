60062842d4c7ccb6974c876cc657958e7be8599d
Kevin Cioch <kevin.cioch@gmail.com>
2023-10-23 19:04:27 +0200

    Implemented popular jobs

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -11,10 +11,14 @@ import { useRouter } from "expo-router";
 import styles from "./popularjobs.style";
 import { COLORS, SIZES } from "../../../constants";
 import PopularJobCard from "../../common/cards/popular/PopularJobCard";
+import useFetch from "../../../hooks/useFetch";
 
 const Popularjobs = () => {
-  const isLoading = false;
-  const error = false;
+  const { data, isLoading, error } = useFetch("search", {
+    query: "React developer",
+    num_pages: 1,
+  });
+
 
   return (
     <View style={styles.container}>
@@ -32,11 +36,11 @@ const Popularjobs = () => {
           <Text>Something went wrong</Text>
         ) : (
           <FlatList
-            data={[1, 2, 3, 4]}
+            data={data}
             contentContainerStyle={{ columnGap: SIZES.medium }}
             horizontal
             showsHorizontalScrollIndicator={false}
-            keyExtractor={(item) => item?.job_id}
+            keyExtractor={(item) => item.job_id}
             renderItem={({ item }) => <PopularJobCard item={item} />}
           />
         )}
